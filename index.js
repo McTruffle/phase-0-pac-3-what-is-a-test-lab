ed5404eee0da414cd7305f08280b2687fd57c322
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-05-26 00:22:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan";
+const height = 39;
+const message = `Hi, my name is ${name}, to include ${height}`;
 
 module.exports = {name, height, message};
+console.log(message);
